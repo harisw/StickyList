0e3d33e134f1489f4ff7b22afa9c754e574d2611
Hamka <hamkaaminullah@yahoo.com>
2017-12-07 15:55:58 +0700

    simple Frame

---
+++ src/Main.java
@@ -0,0 +1,119 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+
+public class Main {
+
+	private JFrame frame;
+	private JPanel[] panel;
+	
+	private int flag;
+	private JTextField textField;
+	private JLabel lblTodoList;
+	private JButton button;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		flag = 0;
+		frame = new JFrame();
+		frame.setBounds(100, 100, 567, 390);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewList = new JButton("New List");
+		btnNewList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lblTodoList.setVisible(true);
+				panel[flag].setVisible(true);
+				flag++;
+			}
+		});
+		btnNewList.setBounds(43, 30, 100, 23);
+		frame.getContentPane().add(btnNewList);
+		
+		panel = new JPanel[5];
+		
+		lblTodoList = new JLabel("To-Do List");
+		lblTodoList.setBounds(30, 64, 58, 14);
+		//lblTodoList.setVisible(false);
+		
+		panel[0] = new JPanel();
+		panel[0].setBounds(30, 64, 105, 263);
+		panel[0].setBackground(Color.WHITE);
+		panel[0].add(lblTodoList);
+		frame.getContentPane().add(panel[0]);
+		panel[0].setVisible(false);
+		
+		panel[1] = new JPanel();
+		panel[1].setBounds(160, 64, 105, 263);
+		frame.getContentPane().add(panel[1]);
+		panel[1].setBackground(Color.RED);
+		panel[1].setVisible(false);
+		
+		panel[2] = new JPanel();
+		panel[2].setBackground(Color.BLUE);
+		panel[2].setBounds(290, 64, 105, 263);
+		frame.getContentPane().add(panel[2]);
+		panel[2].setVisible(false);
+		
+		panel[3] = new JPanel();
+		panel[3].setBackground(Color.GREEN);
+		panel[3].setBounds(420, 64, 105, 263);
+		frame.getContentPane().add(panel[3]);
+		panel[3].setVisible(false);
+		
+		JButton btnDelList = new JButton("Delete List");
+		btnDelList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(flag>=0)
+				{
+					panel[flag].setVisible(false);
+					if(flag!=0) flag--;
+				}
+			}
+		});
+		btnDelList.setBounds(157, 30, 100, 23);
+		frame.getContentPane().add(btnDelList);
+		
+		
+	}
+}
