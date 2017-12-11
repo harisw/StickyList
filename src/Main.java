e198d04abcf48398aeecae1f712aafa765afbf02
DESKTOP-NRNN10Q\har <harrysw6@gmail.com>
2017-12-12 01:14:33 +0700

    - Add mysqlconnector jar - modified gitignore - Add Save workspace and quit menu on toolbar

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ import java.awt.event.*;
 import javax.swing.*; 
 
 public class Main {
-
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JToolBar toolbar;
 	private JFrame frame;
 	private JPanel pan;
 	private JTextField textField;
 	private JButton btnNewList, btnDelList, btnConfirm;
 	private List[] list = new List[5];
 	private int flag = -1;
-
 	/**
 	 * Launch the application.
 	 */
@@ -46,6 +47,31 @@ public class Main {
 		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
 		frame.setIconImage(new ImageIcon(ClassLoader.getSystemResource("images/logo.png")).getImage());
+		
+		//Add new Toolbar
+		menuBar = new JMenuBar();
+		menu = new JMenu("Menu");
+		menu.setMnemonic(KeyEvent.VK_ALT);
+		menuBar.add(menu);
+		
+		JMenuItem menuItem = new JMenuItem("Save Workspace");
+		menuItem.getAccessibleContext().setAccessibleDescription("Save all of your List and "
+				+ "activites into Database");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Save Pressed");
+			}
+		});
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Quit");
+		menuItem.getAccessibleContext().setAccessibleDescription("Exit this Application");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Quit Pressed");
+			}
+		});
+		menu.add(menuItem);
 		
 		btnNewList = new JButton("New List");
 		btnNewList.addActionListener(new ActionListener() {
@@ -96,5 +122,65 @@ public class Main {
 		
 		textField.setVisible(false);
 		btnConfirm.setVisible(false);
+		frame.setJMenuBar(menuBar);
+	}
+	
+	class DropDownButton extends AbstractButton
+	{
+	   JButton actionButton;
+	   JToggleButton menuButton;
+	   JPopupMenu popupMenu;
+	  
+	   public DropDownButton(JButton _actionButton, JPopupMenu _popupMenu) {
+	      this.popupMenu = _popupMenu;
+	      this.actionButton = _actionButton;
+	  
+	      setLayout(new BorderLayout());
+	      actionButton.setBorderPainted(false);
+	      add(BorderLayout.CENTER, actionButton);
+	      menuButton = new JToggleButton(new ImageIcon("down.jpg"));
+	      menuButton.setPreferredSize(new Dimension(15, 10));
+	      add(BorderLayout.EAST, menuButton);
+	      menuButton.setBorderPainted(false);
+	   
+	      MouseAdapter ma = new MouseAdapter() {
+	         public void mouseClicked(MouseEvent me) { }
+	         public void mousePressed(MouseEvent me) {
+	            if (me.getSource() == actionButton) {
+	               menuButton.setSelected(true);
+	            }
+	         }
+	         public void mouseReleased(MouseEvent me) {
+	            if (me.getSource() == actionButton) {
+	               menuButton.setSelected(false);
+	            }
+	         }
+	         public void mouseEntered(MouseEvent me) {
+	            setRolloverBorder();
+	         }
+	         public void mouseExited(MouseEvent me) {
+	            unsetRolloverBorder();
+	         }
+	      };
+	  
+	      actionButton.addMouseListener(ma);
+	      menuButton.addMouseListener(ma);
+	  
+	      menuButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent ae) {
+	            popupMenu.show(actionButton, 0, actionButton.getSize().height);
+	         }
+	      });
+	   } 
+	  
+	   protected void setRolloverBorder() {
+	      actionButton.setBorderPainted(true);
+	      menuButton.setBorderPainted(true);
+	   }
+	  
+	   protected void unsetRolloverBorder() {
+	      actionButton.setBorderPainted(false);
+	      menuButton.setBorderPainted(false);
+	   }
 	}
 }
